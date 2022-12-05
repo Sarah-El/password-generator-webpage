fb20fa8475bf2f3d43c283adba0030d691d2fb07
Sarah-El <sarah-louise.ellis@hotmail.co.uk>
2022-12-05 12:07:15 +0000

    added if statement for if the user charachter length is less than 10 or more that 64

--- starter/script.js
+++ starter/script.js
@@ -92,16 +92,16 @@ var upperCasedCharacters = [
 function getPasswordOptions() {
   var passwordLength = prompt("How many charcters would you like your password to be? Please enter a value between 10 - 64");
   passwordLength = parseInt(passwordLength);
+  if (passwordLength < 10|| passwordLength > 64) {
+    alert("Sorry, your character length must be between 10 and 64 characters. Please start over.")
+    getPasswordOptions ();
+  }
   var includeLowercase = confirm("Would you like your password to include lowercase characters? click 'ok' for yes.");
   var includeUppercase = confirm("Would you like your password to include uppercase characters? click 'ok' for yes.");
   var includeNumber = confirm("Would you like your password to include numbers? click 'ok' for yes.");
   var specialCharacters = confirm("Would you like your password to include special characters? click 'ok' for yes.");
-  console.log("password length =", passwordLength);
-  console.log("type of password length =", typeof(passwordLength));
-  if (passwordLength.length < 10|| passwordLength.length > 64) {
-    alert("Sorry, your character length must be between 10 and 64 characters. Please start over.")
-    getPasswordOptions ();
-  }
+
+
   var passwordOptions = {
     passwordLength : passwordLength,
     includeLowercase : includeLowercase,
@@ -131,22 +131,18 @@ function generatePassword() {
 
   if (userInput.includeLowercase === true){
     validCharacters = validCharacters.concat(lowerCasedCharacters);
-    console.log("valid characters = ", validCharacters);
   }
   if (userInput.includeUppercase === true){
     validCharacters = validCharacters.concat(upperCasedCharacters);
-    console.log("valid characters = ", validCharacters);
   }
   if (userInput.includeNumber === true){
     validCharacters = validCharacters.concat(numericCharacters);
-    console.log("valid characters = ", validCharacters);
   }
   if (userInput.specialCharacters === true){
     validCharacters = validCharacters.concat(specialCharacters);
-    console.log("valid characters = ", validCharacters);
   }
   if (validCharacters.length === 0){
-    alert("At least one character type should be selected. please start over");
+    alert("At least one character type should be selected. Please start over.");
     generatePassword();
   }
 
