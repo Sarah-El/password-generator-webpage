52e88262d8120430ae5aa8e3acf6528991eeed7a
Sarah-El <sarah-louise.ellis@hotmail.co.uk>
2022-12-05 11:35:43 +0000

    added function to generate random user password

--- starter/script.js
+++ starter/script.js
@@ -91,14 +91,26 @@ var upperCasedCharacters = [
 // Function to prompt user for password options
 function getPasswordOptions() {
   var passwordLength = prompt("How many charcters would you like your password to be? Please enter a value between 10 - 64");
+  passwordLength = parseInt(passwordLength);
   var includeLowercase = confirm("Would you like your password to include lowercase characters? click 'ok' for yes.");
   var includeUppercase = confirm("Would you like your password to include uppercase characters? click 'ok' for yes.");
   var includeNumber = confirm("Would you like your password to include numbers? click 'ok' for yes.");
   var specialCharacters = confirm("Would you like your password to include special characters? click 'ok' for yes.");
-  
+  console.log("password length =", passwordLength);
+  console.log("type of password length =", typeof(passwordLength));
   if (passwordLength.length < 10|| passwordLength.length > 64) {
-    alert("Sorry, your character length must be between 10 and 64 characters")
+    alert("Sorry, your character length must be between 10 and 64 characters. Please start over.")
+    getPasswordOptions ();
   }
+  var passwordOptions = {
+    passwordLength : passwordLength,
+    includeLowercase : includeLowercase,
+    includeUppercase : includeUppercase,
+    includeNumber : includeNumber,
+    specialCharacters : specialCharacters
+  }
+
+  return passwordOptions;
 }
 
 
@@ -107,23 +119,44 @@ function getPasswordOptions() {
 function getRandom(arr) {
   var randomIndex = Math.floor(Math.random() * arr.length);
   var random = arr[randomIndex];
-  return randomSelection;  
+  return random;  
 }
 
-var randomResult = getRandom(array);
-
-//function to get random number
-
-function randomNumber (numericCharacters) {
-
-}
 
 // Function to generate password with user input
 function generatePassword() {
   var userInput = getPasswordOptions();
+
+  var validCharacters = [];
+
+  if (userInput.includeLowercase === true){
+    validCharacters = validCharacters.concat(lowerCasedCharacters);
+    console.log("valid characters = ", validCharacters);
+  }
+  if (userInput.includeUppercase === true){
+    validCharacters = validCharacters.concat(upperCasedCharacters);
+    console.log("valid characters = ", validCharacters);
+  }
+  if (userInput.includeNumber === true){
+    validCharacters = validCharacters.concat(numericCharacters);
+    console.log("valid characters = ", validCharacters);
+  }
+  if (userInput.specialCharacters === true){
+    validCharacters = validCharacters.concat(specialCharacters);
+    console.log("valid characters = ", validCharacters);
+  }
+  if (validCharacters.length === 0){
+    alert("At least one character type should be selected. please start over");
+    generatePassword();
+  }
+
+  var randomPassword = "";
+  for (var i = 0; i < userInput.passwordLength; i++) {
+    randomPassword += getRandom(validCharacters);
+    console.log("random password = ", randomPassword);
+  }
+  return randomPassword;
 }
-
-
 
 
 // Get references to the #generate element
